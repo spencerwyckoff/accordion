8f10490aa9da42ac6036d3ea3ed06526ca15dd45
Spencer Wyckoff <spencer.wyckoff@gmail.com>
2015-01-20 16:40:09 -0500

    finished minimum assignment

--- main.js
+++ main.js
@@ -1,6 +1,18 @@
-// Declare Variables
-var body = $('body');
 
-var section = $('section');
-var header = $('.header');
-var p = $('p');
+$('.heading1').on('click', function() {
+	$('.body1').toggleClass('open');
+	$('.body2').removeClass('open');
+	$('.body3').removeClass('open');
+});
+
+$('.heading2').on('click', function() {
+	$('.body2').toggleClass('open');
+	$('.body1').removeClass('open');
+	$('.body3').removeClass('open');
+});
+
+$('.heading3').on('click', function() {
+	$('.body3').toggleClass('open');
+	$('.body1').removeClass('open');
+	$('.body2').removeClass('open');
+});
